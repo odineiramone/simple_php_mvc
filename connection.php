8578d28b6d77b066fbc27945c09727ad14ad17e4
Odinei Rocha <odinei.ribeiro@live.com>
2017-03-26 02:53:42 -0300

    added git ignore and secrets file to sensive data

--- connection.php
+++ connection.php
@@ -1,13 +1,19 @@
 <?php
   class Db {
-    private static $instance = NULL?
+    private static $instance = NULL;
     private function __construct() {}
     private function __clone () {}
 
     public static function getInstance(){
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ATTR_ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=', '', '', $pdo_options);
+
+        $host   = Secrets::getDbHost();
+        $dbname = Secrets::getDbName();
+        $user   = Secrets::getDbUser();
+        $pass   = Secrets::getDbPass();
+
+        self::$instance = new PDO('mysql:host='. $host .';dbname=' . $dbname, $user, $pass, $pdo_options);
       }
       return self::$instance;
     }
