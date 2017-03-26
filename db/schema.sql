d0a6a2134053309f9230db3b26bf58e7dc08d451
Odinei Rocha <odinei.ribeiro@live.com>
2017-03-26 20:11:38 -0300

    added 'if not exist' to .sql file

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ create database if not exists php_mvc;
 
 use php_mvc;
 
-create table posts (
+create table if not exists posts (
   id int(6) auto_increment primary key,
   author varchar(255) not null,
   content text
